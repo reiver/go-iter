56bb9a1f5f53ced622447b085a694608b5843fcf
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-25 10:57:43 -0800

    iter.Float64s

---
+++ float64s.go
@@ -0,0 +1,97 @@
+package iter
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Float64s struct {
+	Slice []float64
+	err error
+	index int
+	datum float64
+	mutex sync.RWMutex
+}
+
+func (receiver *Float64s) Close() error {
+	return nil
+}
+
+// Decode stores the next datum in the data represented by the empty interface value `x`.
+// If `x` is nil, the value will be discarded.
+// Otherwise, the value underlying `x` must be a pointer to the correct type for the next datum.
+func (receiver *Float64s) Decode(x interface{}) error {
+	if nil == receiver {
+		return errNilReceiver
+	}
+
+	receiver.mutex.RLock()
+	defer receiver.mutex.RUnlock()
+
+	p, ok := x.(*float64)
+	if !ok {
+		return &internalBadTypeComplainer{fmt.Sprintf("%T", p)}
+	}
+	if nil == p {
+		return nil
+	}
+
+	*p = receiver.datum
+
+	return nil
+}
+
+// Err returns the error, if an error was encountered during an iteration.
+// If no error was encountered during an iteration, then Err returns nil.
+func (receiver *Float64s) Err() error {
+	if nil == receiver {
+		return errNilReceiver
+	}
+
+	receiver.mutex.RLock()
+	defer receiver.mutex.RUnlock()
+
+	return receiver.err
+}
+
+// Next prepares the next datum for reading via the Decode method.
+//
+// If there is indeed a next datum, Next returns true.
+//
+// And a call to the Decode method will obtain that next datum.
+//
+// If there is no next datum, Next returns false.
+//
+// If an error was encountered while calling Next, Next will also return false.
+//
+// And a call to the Err method will obtain that error.
+//
+// Err should be called to distinguish between the two cases where the Next
+// method can return false: 'no next datum' and 'an error was encountered'.
+func (receiver *Float64s) Next() bool {
+	if nil == receiver {
+		return false
+	}
+
+	receiver.mutex.Lock()
+	defer receiver.mutex.Unlock()
+
+	if nil != receiver.err {
+		return false
+	}
+
+	slice := receiver.Slice
+	if nil == slice  {
+		return false
+	}
+
+	index := receiver.index
+	if len(slice) <= index {
+		return false
+	}
+
+	receiver.datum = slice[index]
+	receiver.index++
+
+	return true
+}
---
+++ float64s_test.go
@@ -0,0 +1,117 @@
+package iter
+
+import (
+	"testing"
+)
+
+func TestFloat64s(t *testing.T) {
+
+	tests := []struct{
+		Slice []float64
+	}{
+		{
+			Slice: []float64{},
+		},
+
+
+
+		{
+			Slice: []float64{0.0},
+		},
+		{
+			Slice: []float64{1.0},
+		},
+		{
+			Slice: []float64{2.0},
+		},
+		{
+			Slice: []float64{3.0},
+		},
+		{
+			Slice: []float64{4.0},
+		},
+		{
+			Slice: []float64{5.0},
+		},
+		{
+			Slice: []float64{6.0},
+		},
+		{
+			Slice: []float64{7.0},
+		},
+		{
+			Slice: []float64{8.0},
+		},
+		{
+			Slice: []float64{9.0},
+		},
+		{
+			Slice: []float64{10.0},
+		},
+
+
+
+		{
+			Slice: []float64{0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0},
+		},
+
+
+
+		{
+			Slice: []float64{213.202,18.179,4.000002},
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		slice := append([]float64(nil), test.Slice...)
+
+		iterator := Float64s{
+			Slice: slice,
+		}
+
+		if err := iterator.Err(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		var actualData []float64
+		iterationNumber := -1
+		for iterator.Next() {
+			iterationNumber++
+
+
+			var datum float64
+
+			if err := iterator.Decode(&datum); nil != err {
+				t.Errorf("For test #%d and iteration #%d, did not expect an error, but actually got one: (%T) %v", testNumber, iterationNumber, err, err)
+				continue
+			}
+
+			actualData = append(actualData, datum)
+		}
+		if err := iterator.Err(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		if expected, actual := len(test.Slice), len(actualData); expected != actual {
+			t.Errorf("For test #%d, expected %d, but actually got %d.", testNumber, expected, actual)
+			continue
+		}
+		for datumNumber, expected := range test.Slice {
+			actual := actualData[datumNumber]
+
+			if expected != actual {
+				t.Errorf("For test #%d and datum #%d, expected %v, but actually got %v.", testNumber, datumNumber, expected, actual)
+				continue
+			}
+		}
+
+		if err := iterator.Close(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+	}
+}
