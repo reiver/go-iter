c449e66d8c9f21c55e5edbde28639479fea3b5a0
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-25 11:31:43 -0800

    docs

--- doc.go
+++ doc.go
@@ -4,6 +4,9 @@ Package iter provides tools for creating iterators.
 These iterators are intentionally made to resemble *sql.Rows from the "database/sql" package.
 Including having the same Close, Err, and Next methods.
 
+(Note that to turn something into an actual *sql.Rows from the "database/sql" package, instead of just resembling it,
+use https://github.com/reiver/go-shunt instead.)
+
 For example, we can turn a slice into an iterator, with code like the following:
 
 	var slice []string = []string {
