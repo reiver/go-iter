cea324d140fdd466281f886a1ce89ff3f945140f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-25 11:11:47 -0800

    iter.Times

---
+++ times_test.go
@@ -0,0 +1,87 @@
+package iter
+
+import (
+	"time"
+
+	"testing"
+)
+
+func TestTimes(t *testing.T) {
+
+	tests := []struct{
+		Slice []time.Time
+	}{
+		{
+			Slice: []time.Time{},
+		},
+
+
+
+		{
+			Slice: []time.Time{ time.Now() },
+		},
+
+
+
+		{
+			Slice: []time.Time{
+				time.Now().Add( -1 * time.Hour ),
+				time.Now().Add( -2 * time.Hour ),
+				time.Now().Add( -3 * time.Hour ),
+			},
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		slice := append([]time.Time(nil), test.Slice...)
+
+		iterator := Times{
+			Slice: slice,
+		}
+
+		if err := iterator.Err(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		var actualData []time.Time
+		iterationNumber := -1
+		for iterator.Next() {
+			iterationNumber++
+
+
+			var datum time.Time
+
+			if err := iterator.Decode(&datum); nil != err {
+				t.Errorf("For test #%d and iteration #%d, did not expect an error, but actually got one: (%T) %v", testNumber, iterationNumber, err, err)
+				continue
+			}
+
+			actualData = append(actualData, datum)
+		}
+		if err := iterator.Err(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		if expected, actual := len(test.Slice), len(actualData); expected != actual {
+			t.Errorf("For test #%d, expected %d, but actually got %d.", testNumber, expected, actual)
+			continue
+		}
+		for datumNumber, expected := range test.Slice {
+			actual := actualData[datumNumber]
+
+			if !expected.Equal(actual) {
+				t.Errorf("For test #%d and datum #%d, expected %v, but actually got %v.", testNumber, datumNumber, expected, actual)
+				continue
+			}
+		}
+
+		if err := iterator.Close(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+	}
+}
