4a92fe67e0d58cbeda049254f01d6d000d08ac9f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-25 15:39:25 -0800

    iter.Uint16

---
+++ uint16.go
@@ -0,0 +1,104 @@
+package iter
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Uint16 struct {
+	Slice []uint16
+	err error
+	index int
+	datum uint16
+	mutex sync.RWMutex
+}
+
+func (receiver *Uint16) Close() error {
+	return nil
+}
+
+// Decode stores the next datum in the data represented by the empty interface value `x`.
+// If `x` is nil, the value will be discarded.
+// Otherwise, the value underlying `x` must be a pointer to the correct type for the next datum.
+func (receiver *Uint16) Decode(x interface{}) error {
+	if nil == receiver {
+		return errNilReceiver
+	}
+
+	receiver.mutex.RLock()
+	defer receiver.mutex.RUnlock()
+
+	switch p := x.(type) {
+	case *uint16:
+		if nil == p {
+			return nil
+		}
+
+		*p = receiver.datum
+		case *interface{}:
+		if nil == p {
+			return nil
+		}
+
+		*p = receiver.datum
+	default:
+		return &internalBadTypeComplainer{fmt.Sprintf("%T", p)}
+	}
+
+	return nil
+}
+
+// Err returns the error, if an error was encountered during an iteration.
+// If no error was encountered during an iteration, then Err returns nil.
+func (receiver *Uint16) Err() error {
+	if nil == receiver {
+		return errNilReceiver
+	}
+
+	receiver.mutex.RLock()
+	defer receiver.mutex.RUnlock()
+
+	return receiver.err
+}
+
+// Next prepares the next datum for reading via the Decode method.
+//
+// If there is indeed a next datum, Next returns true.
+//
+// And a call to the Decode method will obtain that next datum.
+//
+// If there is no next datum, Next returns false.
+//
+// If an error was encountered while calling Next, Next will also return false.
+//
+// And a call to the Err method will obtain that error.
+//
+// Err should be called to distinguish between the two cases where the Next
+// method can return false: 'no next datum' and 'an error was encountered'.
+func (receiver *Uint16) Next() bool {
+	if nil == receiver {
+		return false
+	}
+
+	receiver.mutex.Lock()
+	defer receiver.mutex.Unlock()
+
+	if nil != receiver.err {
+		return false
+	}
+
+	slice := receiver.Slice
+	if nil == slice  {
+		return false
+	}
+
+	index := receiver.index
+	if len(slice) <= index {
+		return false
+	}
+
+	receiver.datum = slice[index]
+	receiver.index++
+
+	return true
+}
---
+++ uint16_test.go
@@ -0,0 +1,148 @@
+package iter
+
+import (
+	"testing"
+)
+
+func TestUint16(t *testing.T) {
+
+	tests := []struct{
+		Slice []uint16
+	}{
+		{
+			Slice: []uint16{},
+		},
+
+
+
+		{
+			Slice: []uint16{0},
+		},
+		{
+			Slice: []uint16{1},
+		},
+		{
+			Slice: []uint16{2},
+		},
+		{
+			Slice: []uint16{3},
+		},
+		{
+			Slice: []uint16{4},
+		},
+		{
+			Slice: []uint16{5},
+		},
+		{
+			Slice: []uint16{6},
+		},
+		{
+			Slice: []uint16{7},
+		},
+		{
+			Slice: []uint16{8},
+		},
+		{
+			Slice: []uint16{9},
+		},
+		{
+			Slice: []uint16{10},
+		},
+
+
+
+		{
+			Slice: []uint16{0,1,2,3,4,5,6,7,8,9,10},
+		},
+
+
+
+		{
+			Slice: []uint16{213,18,4},
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		slice := append([]uint16(nil), test.Slice...)
+
+		iterator := Uint16{
+			Slice: slice,
+		}
+
+		if err := iterator.Err(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		var actualData []uint16
+		iterationNumber := -1
+		for iterator.Next() {
+			iterationNumber++
+
+
+			var datum uint16
+
+			if err := iterator.Decode(&datum); nil != err {
+				t.Errorf("For test #%d and iteration #%d, did not expect an error, but actually got one: (%T) %v", testNumber, iterationNumber, err, err)
+				continue
+			}
+
+			actualData = append(actualData, datum)
+
+
+			if err := iterator.Decode((*uint16)(nil)); nil != err {
+				t.Errorf("For test #%d and iteration #%d, did not expect an error, but actually got one: (%T) %v", testNumber, iterationNumber, err, err)
+				continue
+			}
+
+
+			var x interface{}
+
+			if err := iterator.Decode(&x); nil != err {
+				t.Errorf("For test #%d and iteration #%d, did not expect an error, but actually got one: (%T) %v", testNumber, iterationNumber, err, err)
+				continue
+			}
+
+			datum2, ok := x.(uint16)
+			if !ok {
+				t.Errorf("For test #%d and iteration #%d, expected to be able to cast, but actually could not. (%T)", testNumber, iterationNumber, x)
+				continue
+			}
+
+			if expected, actual := datum, datum2; expected != actual {
+				t.Errorf("For test #%d and iteration #%d, expected %v, but actually got %v.", testNumber, iterationNumber, expected, actual)
+				continue
+			}
+
+
+			if err := iterator.Decode((*interface{})(nil)); nil != err {
+				t.Errorf("For test #%d and iteration #%d, did not expect an error, but actually got one: (%T) %v", testNumber, iterationNumber, err, err)
+				continue
+			}
+		}
+		if err := iterator.Err(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		if expected, actual := len(test.Slice), len(actualData); expected != actual {
+			t.Errorf("For test #%d, expected %d, but actually got %d.", testNumber, expected, actual)
+			continue
+		}
+		for datumNumber, expected := range test.Slice {
+			actual := actualData[datumNumber]
+
+			if expected != actual {
+				t.Errorf("For test #%d and datum #%d, expected %v, but actually got %v.", testNumber, datumNumber, expected, actual)
+				continue
+			}
+		}
+
+		if err := iterator.Close(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+	}
+}
