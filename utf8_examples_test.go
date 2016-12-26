39ebdbbfaf32edb8947a8b82a2717ab5f340d418
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-26 15:59:43 -0800

    added example

---
+++ utf8_examples_test.go
@@ -0,0 +1,49 @@
+package iter_test
+
+
+import (
+	"fmt"
+
+	"github.com/reiver/go-iter"
+)
+
+
+func ExampleUTF8() {
+
+	var p []byte = []byte("Hello world! 🙂")
+
+	iterator := iter.UTF8{Slice:p}
+
+	for iterator.Next() {
+		var r rune
+
+		if err := iterator.Decode(&r); nil != err {
+			fmt.Printf("ERROR: (%T) %v \n", err, err)
+			return
+		}
+
+		fmt.Printf("%q (%d) \n", r, r)
+	}
+	if err := iterator.Err(); nil != err {
+		fmt.Printf("ERROR: (%T) %v \n", err, err)
+		return
+	}
+
+	// Output:
+	// 'H' (72)
+	// 'e' (101)
+	// 'l' (108)
+	// 'l' (108)
+	// 'o' (111)
+	// ' ' (32)
+	// 'w' (119)
+	// 'o' (111)
+	// 'r' (114)
+	// 'l' (108)
+	// 'd' (100)
+	// '!' (33)
+	// ' ' (32)
+	// '🙂' (128578)
+}
+
+
