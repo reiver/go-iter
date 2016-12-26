5b50771f98806621bd1c79cab5fb4faaaf3a4afe
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-26 12:20:02 -0800

    added more tests

---
+++ byte_writeto_test.go
@@ -0,0 +1,113 @@
+package iter
+
+import (
+	"bytes"
+
+	"testing"
+)
+
+func TestByteWriteTo(t *testing.T) {
+
+	tests := []struct{
+		Slice []byte
+	}{
+		{
+			Slice: []byte(nil),
+		},
+		{
+			Slice: []byte{},
+		},
+
+
+
+		{
+			Slice: []byte("apple"),
+		},
+		{
+			Slice: []byte("banana"),
+		},
+		{
+			Slice: []byte("cherry"),
+		},
+
+
+
+		{
+			Slice: []byte("apple banana cherry"),
+		},
+
+
+
+		{
+			Slice: []byte("ONE"),
+		},
+		{
+			Slice: []byte("TWO"),
+		},
+		{
+			Slice: []byte("THREE"),
+		},
+
+
+
+		{
+			Slice: []byte("ONE TWO THREE"),
+		},
+
+
+
+		{
+			Slice: []byte("😐"),
+		},
+
+
+
+		{
+			Slice: []byte("💀👽"),
+		},
+
+	}
+
+
+	for testNumber, test := range tests {
+
+		slice := append([]byte(nil), test.Slice...)
+
+		iterator := Byte{
+			Slice: slice,
+		}
+
+		var buffer bytes.Buffer
+
+		iterationNumber := -1
+		for iterator.Next() {
+			iterationNumber++
+
+			iterator.WriteTo(&buffer)
+
+			var buffer2 bytes.Buffer
+			iterator.WriteTo(&buffer2)
+			if expected, actual := 1, buffer2.Len(); expected != actual {
+				t.Errorf("For test #%d and iteration number #%d, expected %d, but actually got %d.", testNumber, iterationNumber, expected, actual)
+				continue
+			}
+			if expected, actual := test.Slice[iterationNumber], buffer2.Bytes()[0]; expected != actual {
+				t.Errorf("For test #%d and iteration number #%d, expected %d (%q), but actually got %d (%q).", testNumber, iterationNumber, expected, expected, actual, actual)
+				continue
+			}
+		}
+		if err := iterator.Err(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v" , testNumber, err, err)
+			continue
+		}
+
+		if expected, actual := len(test.Slice), len(buffer.Bytes()); expected != actual {
+			t.Errorf("For test #%d, expected %d, but actually got %d." , testNumber, expected, actual)
+			continue
+		}
+		if expected, actual := string(test.Slice), buffer.String(); expected != actual {
+			t.Errorf("For test #%d, expected %q, but actually got %q." , testNumber, expected, actual)
+			continue
+		}
+	}
+}
---
+++ string_writeto_test.go
@@ -0,0 +1,145 @@
+package iter
+
+import (
+	"bytes"
+
+	"testing"
+)
+
+func TestStringWriteTo(t *testing.T) {
+
+	tests := []struct{
+		Slice []string
+	}{
+		{
+			Slice: []string(nil),
+		},
+		{
+			Slice: []string{},
+		},
+
+
+
+		{
+			Slice: []string{"apple"},
+		},
+		{
+			Slice: []string{"banana"},
+		},
+		{
+			Slice: []string{"cherry"},
+		},
+
+
+
+		{
+			Slice: []string{"apple", "banana", "cherry"},
+		},
+
+
+
+		{
+			Slice: []string{"apple banana cherry"},
+		},
+
+
+
+		{
+			Slice: []string{"ONE"},
+		},
+		{
+			Slice: []string{"TWO"},
+		},
+		{
+			Slice: []string{"THREE"},
+		},
+
+
+
+		{
+			Slice: []string{"ONE", "TWO", "THREE"},
+		},
+
+
+
+		{
+			Slice: []string{"ONE TWO THREE"},
+		},
+
+
+
+		{
+			Slice: []string{"😐"},
+		},
+
+
+
+		{
+			Slice: []string{"💀", "👽"},
+		},
+
+
+
+		{
+			Slice: []string{"💀👽"},
+		},
+
+	}
+
+
+	for testNumber, test := range tests {
+
+		slice := append([]string(nil), test.Slice...)
+
+		iterator := String{
+			Slice: slice,
+		}
+
+		var buffer bytes.Buffer
+
+		iterationNumber := -1
+		for iterator.Next() {
+			iterationNumber++
+
+			iterator.WriteTo(&buffer)
+
+			var buffer2 bytes.Buffer
+			iterator.WriteTo(&buffer2)
+			if expected, actual := len(test.Slice[iterationNumber]), buffer2.Len(); expected != actual {
+				t.Errorf("For test #%d and iteration number #%d, expected %d, but actually got %d.", testNumber, iterationNumber, expected, actual)
+				continue
+			}
+			if expected, actual := test.Slice[iterationNumber], buffer2.String(); expected != actual {
+				t.Errorf("For test #%d and iteration number #%d, expected %d (%q), but actually got %d (%q).", testNumber, iterationNumber, expected, expected, actual, actual)
+				continue
+			}
+		}
+		if err := iterator.Err(); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v" , testNumber, err, err)
+			continue
+		}
+
+		{
+			expected := 0
+			for _, s := range test.Slice {
+				expected += len(s)
+			}
+
+			if actual := len(buffer.String()); expected != actual {
+				t.Errorf("For test #%d, expected %d, but actually got %d." , testNumber, expected, actual)
+				continue
+			}
+		}
+		{
+			var expectedBuffer bytes.Buffer
+			for _, s := range test.Slice {
+				expectedBuffer.WriteString(s)
+			}
+
+			if expected, actual := expectedBuffer.String(), buffer.String(); expected != actual {
+				t.Errorf("For test #%d, expected %q, but actually got %q." , testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
