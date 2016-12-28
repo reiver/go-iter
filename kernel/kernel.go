441d23a331d866d28071950c21d60cec829b14af
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-28 15:43:45 -0800

    refactoring. moved method to its own file.

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -71,38 +71,3 @@ func (receiver *Kernel) KernelErr() error {
 
 	return receiver.err
 }
-
-func (receiver *Kernel) KernelNext(fn func(int,interface{})(bool, error)) bool {
-	if nil == receiver {
-		return false
-	}
-
-	receiver.mutex.Lock()
-	defer receiver.mutex.Unlock()
-
-	if nil != receiver.err {
-		return false
-	}
-
-	if receiver.closed {
-		return false
-	}
-
-	index := receiver.index
-
-	var datum interface{}
-
-	next, err := fn(index, &datum)
-	if nil != err {
-		receiver.err = err
-		return false
-	}
-	if !next {
-		return false
-	}
-
-	receiver.datum = datum
-	receiver.index++
-
-	return true
-}
