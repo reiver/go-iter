766675c71a143e82dbbfadfbb88d7ccf72640b0a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-28 15:56:19 -0800

    added more docs

--- kernel/kernel_next.go
+++ kernel/kernel_next.go
@@ -1,5 +1,46 @@
 package iterkernel
 
+// KernelNext is responsible for loading the next value for the iterator.
+//
+// It is the partner of the Next method found in common iterators:
+//
+//	Next() bool
+//
+// (KernelNext does all the "book keeping" for a Next method.)
+//
+// Like Next, KernelNext will return true if there is a next value,
+// and will return false is there is not a next value.
+//
+// Also, like Next, KernelNext returning false could be because of two different reasons.
+//
+// #1 because there are no more values in the iterator.
+//
+// Or #2 because an error occurred while trying to obain the next value.
+//
+// (KernelErr needs to be called to determine which was the case.)
+//
+// The parameter passed to KernelNext is a func with the signature:
+//
+//	func(int, interface{}) (bool, error)
+//
+// This func is what actually (potentially) gives KernelNext the next value.
+//
+// KernelNext calls this func, and passes it #1 the iteration number
+// (which starts counting a zero), and #2 a pointer to where the func
+// should store the next value.
+//
+// If the func loaded a next value successfully, then it should return like the following:
+//
+//	return true, nil
+//
+// If an error occurred while the func was trying to load a next value, then it should return like the following:
+//
+//	return false, err
+//
+// If there is no next value (and no errors occurred) then it should return like the following:
+//
+//	return false, nil
+//
 func (receiver *Kernel) KernelNext(fn func(int,interface{})(bool, error)) bool {
 	if nil == receiver {
 		return false
