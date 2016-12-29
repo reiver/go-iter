cedfdac2bc15592c66e2cabffb604dd74c93f142
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-12-28 16:36:09 -0800

    added more docs

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -6,6 +6,15 @@ import (
 	"sync"
 )
 
+// Kernel deals with the "book keeping" needed when creating a common iterator.
+//
+// For example:
+// making it so calling Close stops Next from being able to load anther value;
+// dealing with having an error that occurred when Next is called get returned  when Err is called;
+// dealing with decoding & scanning into a *interface{},
+// dealing with decoding & scanning into a sql.Scanner,
+// and dealing with the read-write mutex so that this can be safely accessed from multiple threads,
+// etc.
 type Kernel struct {
 	err error
 	index int
