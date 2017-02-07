a2e5cd9a22cdffa8b40856f54dc98d6a5e5892c8
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-02-07 00:52:31 -0800

    decode

---
+++ sqlrows/decode.go
@@ -0,0 +1,294 @@
+package itersqlrows
+
+import (
+	"github.com/reiver/go-cast"
+
+	"database/sql"
+	"fmt"
+	"reflect"
+	"time"
+)
+
+const (
+	targetNameKeyLong   = "iter.target.name"
+	targetNameKeyNormal = "iter.name"
+	targetNameKeyShort  = "iter"
+)
+
+func decode(colScanner columnScanner, v interface{}) error {
+
+	if nil == colScanner {
+		return errNilColumnScanner
+	}
+	if nil == v {
+		return errNilDestination
+	}
+
+
+	data := map[string][]interface{}{}
+	{
+		columns, err := colScanner.Columns()
+		if nil != err {
+			return err
+		}
+
+		var a []interface{}
+		for _, name := range columns {
+			var x interface{} = new(target)
+
+			if _, ok := data[name]; !ok {
+				data[name] = []interface{}{}
+			}
+			data[name] = append(data[name], x)
+
+			a = append(a, x)
+		}
+
+		if err := colScanner.Scan(a...); nil != err {
+			return err
+		}
+	}
+
+
+	var reflectedStructValue reflect.Value
+	var reflectedStructType  reflect.Type
+	{
+		// This needs to get at the struct.
+		reflectedValue := reflect.ValueOf(v)
+		reflectedType  := reflect.TypeOf(v)
+		if nil == reflectedType {
+			return errNilReflectedType
+		}
+		for reflect.Ptr == reflectedValue.Kind() {
+			reflectedValue = reflectedValue.Elem()
+			reflectedType = reflectedType.Elem()
+			if nil == reflectedType {
+				return errNilReflectedType
+			}
+		}
+		if reflect.Struct != reflectedType.Kind() {
+			return fmt.Errorf("Unsupported Type: %T", v)
+		}
+
+		reflectedStructValue = reflectedValue
+		reflectedStructType  = reflectedType
+	}
+
+	numFields := reflectedStructType.NumField()
+	Loop: for fieldNumber:=0; fieldNumber<numFields; fieldNumber++ {
+
+		reflectedFieldValue := reflectedStructValue.Field(fieldNumber)
+		if !reflectedFieldValue.CanSet() {
+			continue Loop
+		}
+
+		// Figure out the "name" the user wants to use.
+		var name string
+		{
+			reflectedStructField := reflectedStructType.Field(fieldNumber)
+
+			var ok bool
+
+			name, ok = reflectedStructField.Tag.Lookup(targetNameKeyLong)
+			if !ok {
+				name, ok = reflectedStructField.Tag.Lookup(targetNameKeyNormal)
+				if !ok {
+					name, ok = reflectedStructField.Tag.Lookup(targetNameKeyShort)
+					if !ok {
+						name = reflectedStructField.Name
+					}
+				}
+			}
+		}
+
+		// Get the value.
+		var value interface{}
+		{
+			values, ok := data[name]
+			if !ok {
+				continue Loop
+			}
+			if 1 > len(values) {
+				continue Loop
+			}
+			valueInterface := values[0]
+			valueTarget, ok := valueInterface.(*target)
+			if !ok {
+				return fmt.Errorf("Unexpected Type: %T", valueTarget)
+			}
+
+			var err error
+			value, err = valueTarget.Interface()
+			if nil != err {
+				return err
+			}
+		}
+
+
+		{
+			pointerToReflectedFieldValue := reflectedFieldValue.Addr()
+			switch x := pointerToReflectedFieldValue.Interface().(type) {
+			case sql.Scanner:
+				if err := x.Scan(value); nil != err {
+					return err
+				}
+				continue Loop
+			}
+		}
+
+
+		var castedValue interface{}
+		{
+			switch reflectedFieldValue.Kind() {
+			case                reflect.Bool:
+				casted, err := cast.Bool(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Complex64:
+				casted, err := cast.Complex64(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Complex128:
+				casted, err := cast.Complex128(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Float32:
+				casted, err := cast.Float32(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Float64:
+				casted, err := cast.Float64(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Int:
+				casted, err := cast.Int(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Int8:
+				casted, err := cast.Int8(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Int16:
+				casted, err := cast.Int16(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Int32:
+				casted, err := cast.Int32(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Int64:
+				casted, err := cast.Int64(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.String:
+				casted, err := cast.String(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Uint:
+				casted, err := cast.Uint(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Uint8:
+				casted, err := cast.Uint8(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Uint16:
+				casted, err := cast.Uint16(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Uint32:
+				casted, err := cast.Uint32(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+			case                reflect.Uint64:
+				casted, err := cast.Uint64(value)
+				if nil != err {
+					err = errCouldNotSet(casted, name, err)
+					return err
+				}
+				castedValue = casted
+
+			case reflect.Slice:
+				switch casted := value.(type) {
+				case []byte:
+					castedValue = casted
+				default:
+					err := fmt.Errorf("Cannot cast into something of type %T, for struct field target name %q.", value, name)
+					return err
+				}
+			case reflect.Struct:
+				switch casted := value.(type) {
+				case time.Time:
+					castedValue = casted
+				default:
+					err := fmt.Errorf("Cannot cast into something of type %T, for struct field target name %q.", value, name)
+					return err
+				}
+
+			default:
+				err := fmt.Errorf("Cannot cast into something of type %T, for struct field target name %q.", value, name)
+				return err
+			}
+		}
+
+		if err := func() (err error) {
+			defer func() {
+				if r := recover(); nil != r {
+					err = fmt.Errorf("Could not set value ([%T] %v) for struct field named %q because: (%T) %v", castedValue, castedValue, name, r, r)
+				}
+			}()
+			reflectedFieldValue.Set( reflect.ValueOf(castedValue) )
+			return nil
+		}(); nil != err {
+			return err
+		}
+	}
+
+
+	return nil
+}
---
+++ sqlrows/decode_test.go
@@ -0,0 +1,92 @@
+package itersqlrows
+
+import (
+	"time"
+
+	"testing"
+)
+
+func TestDecode(t *testing.T) {
+
+	now := time.Now()
+
+	colScanner := &mockColumnScanner{
+		ColumnNames: []string{
+			"fruit_name",
+			"name", "age",
+			"height",
+			"is_female",
+			"when_created",
+			"file_data",
+			"greeting",
+		},
+		ColumnsErr:    nil,
+		ScanValues:  []interface{}{
+			"cherry",
+			"Wanda Doe",
+			int64(23),
+			float64(5.2),
+			true,
+			now,
+			[]byte("apple banana cherry"),
+			"Hello world!",
+		},
+	}
+
+	type MyStruct struct {
+		FruitName   string    `iter:"fruit_name"`
+		PersonName  string    `iter.name:"name"`
+		Age         int64     `iter.target.name:"age"`
+		Height      float64   `iter:"height"`
+		IsFemale    bool      `iter.name:"is_female"`
+		WhenCreated time.Time `iter.target.name:"when_created"`
+		File      []byte      `iter:"file_data"`
+		Greeting    target    `iter.name:"greeting"`
+	}
+
+	var myStruct MyStruct
+
+	if err := decode(colScanner, &myStruct); nil != err {
+		t.Errorf("Did not expect an error, but actually got one: (%T) %v", err, err)
+		return
+	}
+
+	if expected, actual := "cherry", myStruct.FruitName; expected != actual {
+		t.Errorf("Expected (%T) ⟨%v⟩, but actually got (%T) ⟨%v⟩.", expected, expected, actual, actual)
+		return
+	}
+	if expected, actual := "Wanda Doe", myStruct.PersonName; expected != actual {
+		t.Errorf("Expected (%T) ⟨%v⟩, but actually got (%T) ⟨%v⟩.", expected, expected, actual, actual)
+		return
+	}
+	if expected, actual := int64(23), myStruct.Age; expected != actual {
+		t.Errorf("Expected (%T) ⟨%v⟩, but actually got (%T) ⟨%v⟩.", expected, expected, actual, actual)
+		return
+	}
+	if expected, actual := float64(5.2), myStruct.Height; expected != actual {
+		t.Errorf("Expected (%T) ⟨%v⟩, but actually got (%T) ⟨%v⟩.", expected, expected, actual, actual)
+		return
+	}
+	if expected, actual := true, myStruct.IsFemale; expected != actual {
+		t.Errorf("Expected (%T) ⟨%v⟩, but actually got (%T) ⟨%v⟩.", expected, expected, actual, actual)
+		return
+	}
+	{
+		i, err := myStruct.Greeting.Interface()
+		if nil != err {
+			t.Errorf("Did not expect an error, but actually got one: (%T) %v", err, err)
+			return
+		}
+
+		s, ok := i.(string)
+		if !ok {
+			t.Errorf("Expected string, but actually got %t", i)
+			return
+		}
+
+		if expected, actual := "Hello world!", s; expected != actual {
+			t.Errorf("Expected (%T) ⟨%v⟩, but actually got (%T) ⟨%v⟩.", expected, expected, actual, actual)
+			return
+		}
+	}
+}
