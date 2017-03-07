8f41e6603b3ad9c640332203c7ffe39f33c81bb5
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-03-06 16:17:23 -0800

    this should have been committed before.

---
+++ errors.go
@@ -0,0 +1,14 @@
+package iter
+
+import (
+	"errors"
+)
+
+var (
+	errEmptyIterator     = errors.New("Empty Iterator")
+	errInternalError     = errors.New("Internal Error")
+	errNilIterator       = errors.New("Nil Iterator")
+	errNilReceiver       = errors.New("Nil Receiver")
+	errNotFunc           = errors.New("Not Func")
+	errTooManyIterations = errors.New("Too Many Iterations")
+)
