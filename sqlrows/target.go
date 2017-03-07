8f36fe20171ea0b3f4cc7b001cc2b8d5c25823a7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-03-06 18:39:57 -0800

    made "target" type also support various pointer types

--- sqlrows/target.go
+++ sqlrows/target.go
@@ -6,24 +6,38 @@ import (
 )
 
 const (
-	targetTypeBool    = "bool"
-	targetTypeBytes   = "[]byte"
-	targetTypeFloat32 = "float32"
-	targetTypeFloat64 = "float64"
-	targetTypeInt     = "int"
-	targetTypeInt8    = "int8"
-	targetTypeInt16   = "int16"
-	targetTypeInt32   = "int32"
-	targetTypeInt64   = "int64"
-	targetTypeNil     = "nil"
-	targetTypeString  = "string"
-	targetTypeTime    = "time.Time"
-	targetTypeTimePtr = "*time.Time"
-	targetTypeUint    = "uint"
-	targetTypeUint8   = "uint8"
-	targetTypeUint16  = "uint16"
-	targetTypeUint32  = "uint32"
-	targetTypeUint64  = "uint64"
+	targetTypeBool       =  "bool"
+	targetTypeBoolPtr    = "*bool"
+	targetTypeBytes      = "[]byte"
+	targetTypeFloat32    =  "float32"
+	targetTypeFloat32Ptr = "*float32"
+	targetTypeFloat64    =  "float64"
+	targetTypeFloat64Ptr = "*float64"
+	targetTypeInt        =  "int"
+	targetTypeIntPtr     = "*int"
+	targetTypeInt8       =  "int8"
+	targetTypeInt8Ptr    = "*int8"
+	targetTypeInt16      =  "int16"
+	targetTypeInt16Ptr   = "*int16"
+	targetTypeInt32      =  "int32"
+	targetTypeInt32Ptr   = "*int32"
+	targetTypeInt64      =  "int64"
+	targetTypeInt64Ptr   = "*int64"
+	targetTypeNil        = "nil"
+	targetTypeString     =  "string"
+	targetTypeStringPtr  = "*string"
+	targetTypeTime       =  "time.Time"
+	targetTypeTimePtr    = "*time.Time"
+	targetTypeUint       =  "uint"
+	targetTypeUintPtr    = "*uint"
+	targetTypeUint8      =  "uint8"
+	targetTypeUint8Ptr   = "*uint8"
+	targetTypeUint16     =  "uint16"
+	targetTypeUint16Ptr  = "*uint16"
+	targetTypeUint32     =  "uint32"
+	targetTypeUint32Ptr  = "*uint32"
+	targetTypeUint64     =  "uint64"
+	targetTypeUint64Ptr  = "*uint64"
 )
 
 // target represents something that can be either a bool, []byte, float64, int64, string, or time.Time.
@@ -34,24 +48,38 @@ const (
 type target struct {
 	typ string
 
-	boolValue     bool
-	bytesValue  []byte
-	float32Value  float32
-	float64Value  float64
-	intValue      int
-	int8Value     int8
-	int16Value    int16
-	int32Value    int32
-	int64Value    int64
-	nilValue      interface{}
-	stringValue   string
-	timeValue     time.Time
-	timePtrValue *time.Time
-	uintValue     uint
-	uint8Value    uint8
-	uint16Value   uint16
-	uint32Value   uint32
-	uint64Value   uint64
+	boolValue        bool
+	boolPtrValue    *bool
+	bytesValue     []byte
+	float32Value     float32
+	float32PtrValue *float32
+	float64Value     float64
+	float64PtrValue *float64
+	intValue         int
+	intPtrValue     *int
+	int8Value        int8
+	int8PtrValue    *int8
+	int16Value       int16
+	int16PtrValue   *int16
+	int32Value       int32
+	int32PtrValue   *int32
+	int64Value       int64
+	int64PtrValue   *int64
+	nilValue         interface{}
+	stringValue      string
+	stringPtrValue  *string
+	timeValue        time.Time
+	timePtrValue    *time.Time
+	uintValue        uint
+	uintPtrValue    *uint
+	uint8Value       uint8
+	uint8PtrValue   *uint8
+	uint16Value      uint16
+	uint16PtrValue  *uint16
+	uint32Value      uint32
+	uint32PtrValue  *uint32
+	uint64Value      uint64
+	uint64PtrValue  *uint64
 }
 
 
@@ -61,40 +89,68 @@ func (receiver target) Interface() (interface{}, error) {
 	switch t {
 	case targetTypeBool:
 		return receiver.boolValue, nil
+	case targetTypeBoolPtr:
+		return receiver.boolPtrValue, nil
 	case targetTypeBytes:
 		return receiver.bytesValue, nil
 	case targetTypeFloat32:
 		return receiver.float32Value, nil
+	case targetTypeFloat32Ptr:
+		return receiver.float32PtrValue, nil
 	case targetTypeFloat64:
 		return receiver.float64Value, nil
+	case targetTypeFloat64Ptr:
+		return receiver.float64PtrValue, nil
 	case targetTypeInt:
 		return receiver.intValue, nil
+	case targetTypeIntPtr:
+		return receiver.intPtrValue, nil
 	case targetTypeInt8:
 		return receiver.int8Value, nil
+	case targetTypeInt8Ptr:
+		return receiver.int8PtrValue, nil
 	case targetTypeInt16:
 		return receiver.int16Value, nil
+	case targetTypeInt16Ptr:
+		return receiver.int16PtrValue, nil
 	case targetTypeInt32:
 		return receiver.int32Value, nil
+	case targetTypeInt32Ptr:
+		return receiver.int32PtrValue, nil
 	case targetTypeInt64:
 		return receiver.int64Value, nil
+	case targetTypeInt64Ptr:
+		return receiver.int64PtrValue, nil
 	case targetTypeNil:
 		return receiver.nilValue, nil
 	case targetTypeString:
 		return receiver.stringValue, nil
+	case targetTypeStringPtr:
+		return receiver.stringPtrValue, nil
 	case targetTypeTime:
 		return receiver.timeValue, nil
 	case targetTypeTimePtr:
 		return receiver.timePtrValue, nil
 	case targetTypeUint:
 		return receiver.uintValue, nil
+	case targetTypeUintPtr:
+		return receiver.uintPtrValue, nil
 	case targetTypeUint8:
 		return receiver.uint8Value, nil
+	case targetTypeUint8Ptr:
+		return receiver.uint8PtrValue, nil
 	case targetTypeUint16:
 		return receiver.uint16Value, nil
+	case targetTypeUint16Ptr:
+		return receiver.uint16PtrValue, nil
 	case targetTypeUint32:
 		return receiver.uint32Value, nil
+	case targetTypeUint32Ptr:
+		return receiver.uint32PtrValue, nil
 	case targetTypeUint64:
 		return receiver.uint64Value, nil
+	case targetTypeUint64Ptr:
+		return receiver.uint64PtrValue, nil
 	default:
 		return nil, fmt.Errorf("Unsupported Type: %q", t)
 	}
@@ -110,6 +166,10 @@ func (receiver *target) Scan(src interface{}) error {
 		receiver.typ = targetTypeBool
 		receiver.boolValue = t
 		return nil
+	case *bool:
+		receiver.typ = targetTypeBoolPtr
+		receiver.boolPtrValue = t
+		return nil
 	case []byte:
 		receiver.typ = targetTypeBytes
 		receiver.bytesValue = t
@@ -118,33 +178,65 @@ func (receiver *target) Scan(src interface{}) error {
 		receiver.typ = targetTypeFloat32
 		receiver.float32Value = t
 		return nil
+	case *float32:
+		receiver.typ = targetTypeFloat32Ptr
+		receiver.float32PtrValue = t
+		return nil
 	case float64:
 		receiver.typ = targetTypeFloat64
 		receiver.float64Value = t
+		return nil
+	case *float64:
+		receiver.typ = targetTypeFloat64Ptr
+		receiver.float64PtrValue = t
 		return nil
 	case int:
 		receiver.typ = targetTypeInt
 		receiver.intValue = t
 		return nil
+	case *int:
+		receiver.typ = targetTypeIntPtr
+		receiver.intPtrValue = t
+		return nil
 	case int8:
 		receiver.typ = targetTypeInt8
 		receiver.int8Value = t
+		return nil
+	case *int8:
+		receiver.typ = targetTypeInt8Ptr
+		receiver.int8PtrValue = t
 		return nil
 	case int16:
 		receiver.typ = targetTypeInt16
 		receiver.int16Value = t
 		return nil
+	case *int16:
+		receiver.typ = targetTypeInt16Ptr
+		receiver.int16PtrValue = t
+		return nil
 	case int32:
 		receiver.typ = targetTypeInt32
 		receiver.int32Value = t
+		return nil
+	case *int32:
+		receiver.typ = targetTypeInt32Ptr
+		receiver.int32PtrValue = t
 		return nil
 	case int64:
 		receiver.typ = targetTypeInt64
 		receiver.int64Value = t
 		return nil
+	case *int64:
+		receiver.typ = targetTypeInt64Ptr
+		receiver.int64PtrValue = t
+		return nil
 	case string:
 		receiver.typ = targetTypeString
 		receiver.stringValue = t
+		return nil
+	case *string:
+		receiver.typ = targetTypeStringPtr
+		receiver.stringPtrValue = t
 		return nil
 	case time.Time:
 		receiver.typ = targetTypeTime
@@ -158,21 +250,41 @@ func (receiver *target) Scan(src interface{}) error {
 		receiver.typ = targetTypeUint
 		receiver.uintValue = t
 		return nil
+	case *uint:
+		receiver.typ = targetTypeUintPtr
+		receiver.uintPtrValue = t
+		return nil
 	case uint8:
 		receiver.typ = targetTypeUint8
 		receiver.uint8Value = t
+		return nil
+	case *uint8:
+		receiver.typ = targetTypeUint8Ptr
+		receiver.uint8PtrValue = t
 		return nil
 	case uint16:
 		receiver.typ = targetTypeUint16
 		receiver.uint16Value = t
 		return nil
+	case *uint16:
+		receiver.typ = targetTypeUint16Ptr
+		receiver.uint16PtrValue = t
+		return nil
 	case uint32:
 		receiver.typ = targetTypeUint32
 		receiver.uint32Value = t
 		return nil
+	case *uint32:
+		receiver.typ = targetTypeUint32Ptr
+		receiver.uint32PtrValue = t
+		return nil
 	case uint64:
 		receiver.typ = targetTypeUint64
 		receiver.uint64Value = t
+		return nil
+	case *uint64:
+		receiver.typ = targetTypeUint64Ptr
+		receiver.uint64PtrValue = t
 		return nil
 	default:
 		if nil == t {
@@ -192,40 +304,68 @@ func (receiver *target) String() string {
 	switch receiver.typ {
 	case targetTypeBool:
 		return fmt.Sprintf("%v", receiver.boolValue)
+	case targetTypeBoolPtr:
+		return fmt.Sprintf("%v", receiver.boolPtrValue)
 	case targetTypeBytes:
 		return fmt.Sprintf("%v", receiver.bytesValue)
 	case targetTypeFloat32:
 		return fmt.Sprintf("%v", receiver.float32Value)
+	case targetTypeFloat32Ptr:
+		return fmt.Sprintf("%v", receiver.float32PtrValue)
 	case targetTypeFloat64:
 		return fmt.Sprintf("%v", receiver.float64Value)
+	case targetTypeFloat64Ptr:
+		return fmt.Sprintf("%v", receiver.float64PtrValue)
 	case targetTypeInt:
 		return fmt.Sprintf("%v", receiver.intValue)
+	case targetTypeIntPtr:
+		return fmt.Sprintf("%v", receiver.intPtrValue)
 	case targetTypeInt8:
 		return fmt.Sprintf("%v", receiver.int8Value)
+	case targetTypeInt8Ptr:
+		return fmt.Sprintf("%v", receiver.int8PtrValue)
 	case targetTypeInt16:
 		return fmt.Sprintf("%v", receiver.int16Value)
+	case targetTypeInt16Ptr:
+		return fmt.Sprintf("%v", receiver.int16PtrValue)
 	case targetTypeInt32:
 		return fmt.Sprintf("%v", receiver.int32Value)
+	case targetTypeInt32Ptr:
+		return fmt.Sprintf("%v", receiver.int32PtrValue)
 	case targetTypeInt64:
 		return fmt.Sprintf("%v", receiver.int64Value)
+	case targetTypeInt64Ptr:
+		return fmt.Sprintf("%v", receiver.int64PtrValue)
 	case targetTypeNil:
 		return fmt.Sprintf("%v", receiver.nilValue)
 	case targetTypeString:
 		return fmt.Sprintf("%v", receiver.stringValue)
+	case targetTypeStringPtr:
+		return fmt.Sprintf("%v", receiver.stringPtrValue)
 	case targetTypeTime:
 		return fmt.Sprintf("%v", receiver.timeValue)
 	case targetTypeTimePtr:
 		return fmt.Sprintf("%v", receiver.timePtrValue)
 	case targetTypeUint:
 		return fmt.Sprintf("%v", receiver.uintValue)
+	case targetTypeUintPtr:
+		return fmt.Sprintf("%v", receiver.uintPtrValue)
 	case targetTypeUint8:
 		return fmt.Sprintf("%v", receiver.uint8Value)
+	case targetTypeUint8Ptr:
+		return fmt.Sprintf("%v", receiver.uint8PtrValue)
 	case targetTypeUint16:
 		return fmt.Sprintf("%v", receiver.uint16Value)
+	case targetTypeUint16Ptr:
+		return fmt.Sprintf("%v", receiver.uint16PtrValue)
 	case targetTypeUint32:
 		return fmt.Sprintf("%v", receiver.uint32Value)
+	case targetTypeUint32Ptr:
+		return fmt.Sprintf("%v", receiver.uint32PtrValue)
 	case targetTypeUint64:
 		return fmt.Sprintf("%v", receiver.uint64Value)
+	case targetTypeUint64Ptr:
+		return fmt.Sprintf("%v", receiver.uint64PtrValue)
 	default:
 		panic(fmt.Errorf("Internal Error: Unsupported Type: %T", receiver.typ))
 	}
---
+++ sqlrows/target_ptr_test.go
@@ -0,0 +1,857 @@
+package itersqlrows
+
+import (
+	"math"
+	"time"
+
+	"testing"
+)
+
+func TestTargetBoolPtr(t *testing.T) {
+
+	boolFalse := false
+	boolTrue  := true
+
+	tests := []struct{
+		Value *bool
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &boolFalse,
+		},
+		{
+			Value: &boolTrue,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *bool:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetFloat32Ptr(t *testing.T) {
+
+	negativeMax             := float32(-math.MaxFloat32)
+	negativeOne             := float32(-1.0)
+	negativeSmallestNonzero := float32(-math.SmallestNonzeroFloat32)
+	zero                    := float32(0.0)
+	smallestNonzero         := float32(math.SmallestNonzeroFloat32)
+	one                     := float32(1.0)
+	max                     := float32(math.MaxFloat32)
+
+	tests := []struct{
+		Value *float32
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &negativeMax,
+		},
+		{
+			Value: &negativeOne,
+		},
+		{
+			Value: &negativeSmallestNonzero,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &smallestNonzero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *float32:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetFloat64Ptr(t *testing.T) {
+
+	negativeMax             := -math.MaxFloat64
+	negativeOne             := -1.0
+	negativeSmallestNonzero := -math.SmallestNonzeroFloat64
+	zero                    := 0.0
+	smallestNonzero         := math.SmallestNonzeroFloat64
+	one                     := 1.0
+	max                     := math.MaxFloat64
+
+	tests := []struct{
+		Value *float64
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &negativeMax,
+		},
+		{
+			Value: &negativeOne,
+		},
+		{
+			Value: &negativeSmallestNonzero,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &smallestNonzero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *float64:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetIntPtr(t *testing.T) {
+
+	min         := int(math.MinInt32)
+	negativeOne := int(-1)
+	zero        := int(0)
+	one         := int(1)
+	max         := int(math.MaxInt32)
+
+	tests := []struct{
+		Value *int
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &min,
+		},
+		{
+			Value: &negativeOne,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *int:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetInt8Ptr(t *testing.T) {
+
+	min         := int8(math.MinInt8)
+	negativeOne := int8(-1)
+	zero        := int8(0)
+	one         := int8(1)
+	max         := int8(math.MaxInt8)
+
+	tests := []struct{
+		Value *int8
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &min,
+		},
+		{
+			Value: &negativeOne,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *int8:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetInt16Ptr(t *testing.T) {
+
+	min         := int16(math.MinInt16)
+	negativeOne := int16(-1)
+	zero        := int16(0)
+	one         := int16(1)
+	max         := int16(math.MaxInt16)
+
+	tests := []struct{
+		Value *int16
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &min,
+		},
+		{
+			Value: &negativeOne,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *int16:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetInt32Ptr(t *testing.T) {
+
+	min         := int32(math.MinInt32)
+	negativeOne := int32(-1)
+	zero        := int32(0)
+	one         := int32(1)
+	max         := int32(math.MaxInt32)
+
+	tests := []struct{
+		Value *int32
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &min,
+		},
+		{
+			Value: &negativeOne,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *int32:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetInt64Ptr(t *testing.T) {
+
+	min         := int64(math.MinInt64)
+	negativeOne := int64(-1)
+	zero        := int64(0)
+	one         := int64(1)
+	max         := int64(math.MaxInt64)
+
+	tests := []struct{
+		Value *int64
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &min,
+		},
+		{
+			Value: &negativeOne,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *int64:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetStringPtr(t *testing.T) {
+
+	apple      := "apple"
+	banana     := "banana"
+	cherry     := "cherry"
+	date       := "date"
+	helloWorld := "Hello world"
+
+	tests := []struct{
+		Value *string
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &apple,
+		},
+		{
+			Value: &banana,
+		},
+		{
+			Value: &cherry,
+		},
+		{
+			Value: &date,
+		},
+		{
+			Value: &helloWorld,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *string:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetTimePtr(t *testing.T) {
+
+	t0 := time.Now()
+	t1 := time.Now().Add(1 * time.Millisecond)
+	t2 := time.Now().Add(2 * time.Second)
+	t3 := time.Now().Add(3 * time.Minute)
+	t4 := time.Now().Add(4 * time.Hour)
+
+	tests := []struct{
+		Value *time.Time
+	}{
+		{
+			Value: nil,
+		},
+		{
+			&t0,
+		},
+		{
+			Value: &t1,
+		},
+		{
+			Value: &t2,
+		},
+		{
+			Value: &t3,
+		},
+		{
+			Value: &t4,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *time.Time:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetUintPtr(t *testing.T) {
+
+	zero := uint(0)
+	one  := uint(1)
+	max  := uint(math.MaxUint32)
+
+	tests := []struct{
+		Value *uint
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *uint:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetUint8Ptr(t *testing.T) {
+
+	zero := uint8(0)
+	one  := uint8(1)
+	max  := uint8(math.MaxUint8)
+
+	tests := []struct{
+		Value *uint8
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *uint8:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetUint16Ptr(t *testing.T) {
+
+	zero := uint16(0)
+	one  := uint16(1)
+	max  := uint16(math.MaxUint16)
+
+	tests := []struct{
+		Value *uint16
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *uint16:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetUint32Ptr(t *testing.T) {
+
+	zero := uint32(0)
+	one  := uint32(1)
+	max  := uint32(math.MaxUint32)
+
+	tests := []struct{
+		Value *uint32
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *uint32:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
+
+func TestTargetUint64Ptr(t *testing.T) {
+
+	zero := uint64(0)
+	one  := uint64(1)
+	max  := uint64(math.MaxUint64)
+
+	tests := []struct{
+		Value *uint64
+	}{
+		{
+			Value: nil,
+		},
+		{
+			Value: &zero,
+		},
+		{
+			Value: &one,
+		},
+		{
+			Value: &max,
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		var x target
+
+		if err := x.Scan(test.Value); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+
+		i, err := x.Interface()
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %v", testNumber, err, err)
+			continue
+		}
+		switch casted := i.(type) {
+		default:
+			t.Errorf("For test #%d, unexpected type: %T", testNumber, i)
+			continue
+		case *uint64:
+			if expected, actual := test.Value, casted; expected != actual {
+				t.Errorf("For test #%d, expected %v, but actually got %v.", testNumber, expected, actual)
+				continue
+			}
+		}
+	}
+}
