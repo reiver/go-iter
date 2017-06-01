e54a08e45bdbba6c6fbc2cc38824aeb76d88d8a4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2017-06-01 11:28:28 -0700

    added docs

--- sqlrows/doc.go
+++ sqlrows/doc.go
@@ -1,4 +1,14 @@
 /*
 Package itersqlrows provides tools for creating iterators for *sql.Rows from the standard Go package "database/sql".
+
+Example
+
+	var rows *sql.Rows
+	
+	// ...
+	
+	iterator := itersqlrows.Rows{
+		Rows: rows,
+	}
 */
 package itersqlrows
